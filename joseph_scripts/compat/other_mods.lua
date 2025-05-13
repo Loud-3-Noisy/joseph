83b10784cce13d2636bd583bede0877cac0691ed
Kattack <29678669+Kattacka@users.noreply.github.com>
2025-05-12 20:13:12 -0400

    Added unique birthrights support

--- joseph_scripts/compat/other_mods.lua
+++ joseph_scripts/compat/other_mods.lua
@@ -3,15 +3,51 @@ local other_mods = {}
 local enums = JosephMod.enums
 local mod = JosephMod
 
-
-
-
-function mod:CompatScripts()
+function other_mods:CompatScripts()
     if TheFuture then
         TheFuture.ModdedCharacterDialogue["Joseph​"] = {
             "you look like you know your way around cards",
             "...",
-            "up for a game of four souls?", }
+            "up for a game of four souls?",
+        }
     end
+
+    if UniqueItemsAPI then
+        UniqueItemsAPI.RegisterMod("Joseph")
+        UniqueItemsAPI.RegisterCharacter("Joseph​", false, "Joseph")
+        UniqueItemsAPI.AssignUniqueObject({
+            PlayerType = enums.PlayerType.PLAYER_JOSEPH,
+            ObjectID = CollectibleType.COLLECTIBLE_BIRTHRIGHT,
+            SpritePath = {"gfx/items/collectibles/joseph_birthright.png"},
+            GlobalMod = false
+        }, UniqueItemsAPI.ObjectType.COLLECTIBLE)
+    end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
-mod:AddCallback(ModCallbacks.MC_POST_MODS_LOADED, mod.CompatScripts)
+mod:AddCallback(ModCallbacks.MC_POST_MODS_LOADED, other_mods.CompatScripts)
